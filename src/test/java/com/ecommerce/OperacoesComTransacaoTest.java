9e7496efa583cc24e9bf245e171c2475e5eb9fda
akirack <couzack_2009@hotmail.com>
2022-08-27 13:49:32 -0300

    Delete e update

--- src/test/java/com/ecommerce/OperacoesComTransacaoTest.java
+++ src/test/java/com/ecommerce/OperacoesComTransacaoTest.java
@@ -9,11 +9,44 @@ import java.math.BigDecimal;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
+    public void atualizaObjeto(){
+
+        Produto produto = new Produto();
+
+        produto.setId(1);
+        produto.setNome("AAAAAAA");
+        produto.setDescricao("dsadsadsa");
+        produto.setPreco(new BigDecimal(900.62));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertNotNull(produtoVerificacao);
+
+    }
+
+    @Test
+    public void removerObjeto(){
+        Produto produto = entityManager.find(Produto.class, 3);
+
+        entityManager.getTransaction().begin();
+        entityManager.remove(produto);
+        entityManager.getTransaction().commit();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, 3);
+        Assert.assertNull(produtoVerificacao);
+    }
+
+    @Test
     public void inserirPrimeiroObjeto(){
         Produto produto = new Produto();
-        produto.setId(2);
-        produto.setNome("Camera Canon");
-        produto.setPreco(new BigDecimal(399.0));
+        produto.setId(3);
+        produto.setNome("Camera Canon aaaa");
+        produto.setPreco(new BigDecimal(329.0));
         produto.setDescricao("Certamente é uma camera");
 
         entityManager.getTransaction().begin();
